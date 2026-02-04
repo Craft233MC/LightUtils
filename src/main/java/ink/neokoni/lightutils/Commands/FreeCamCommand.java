ef8543007e5f64745af9a015fb02f860e41f835b
neokoni <dulin081682@126.com>
2026-02-05 01:44:48 +0800

    fix: load freecam data from playerData
    Signed-off-by: neokoni <dulin081682@126.com>

--- src/main/java/ink/neokoni/lightutils/Commands/FreeCamCommand.java
+++ src/main/java/ink/neokoni/lightutils/Commands/FreeCamCommand.java
@@ -10,17 +10,22 @@ import ink.neokoni.lightutils.Tasks.FreeCamDetectPlayersDistanceTask;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
+import java.nio.Buffer;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class FreeCamCommand {
     private final LightUtils plugin = LightUtils.getInstance();
-    private static final Map<Player, Pair<Location, GameMode>> enabled = new ConcurrentHashMap<>();
+    private static final Map<OfflinePlayer, Pair<Location, GameMode>> enabled = new ConcurrentHashMap<>();
 
     public FreeCamCommand() {
         plugin.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
@@ -33,6 +38,16 @@ public class FreeCamCommand {
                                 Configs.getConfigs().getInt("freecam.limit"));
                     });
             commands.registrar().register(command.build());
+        });
+
+        YamlConfiguration playerData = PlayerDatas.getPlayerData();
+        playerData.getKeys(false).forEach(uuid -> {
+            if (playerData.isSet(uuid+".freecam.gamemode") && playerData.isSet(uuid+".freecam.location")) {
+                OfflinePlayer player = Bukkit.getServer().getOfflinePlayer(UUID.fromString(uuid));
+                Location location = playerData.getObject(uuid+".freecam.location", Location.class);
+                GameMode gameMode = GameMode.valueOf(playerData.getString(uuid+".freecam.gamemode"));
+                enabled.put(player, Pair.of(location, gameMode));
+            }
         });
     }
 
@@ -61,6 +76,7 @@ public class FreeCamCommand {
             playerData.set(player.getUniqueId()+".freecam.location", null);
 
             PlayerDatas.savePlayerData(playerData);
+            PlayerDatas.writeToFile();
 
             player.setGameMode(enabled.get(player).getSecond());
             player.teleportAsync(enabled.get(player).getFirst());
@@ -71,6 +87,7 @@ public class FreeCamCommand {
             playerData.set(player.getUniqueId()+".freecam.location", player.getLocation());
 
             PlayerDatas.savePlayerData(playerData);
+            PlayerDatas.writeToFile();
 
             player.setGameMode(GameMode.SPECTATOR);
 
@@ -82,7 +99,7 @@ public class FreeCamCommand {
         return Command.SINGLE_SUCCESS;
     }
 
-    public static Map<Player, Pair<Location, GameMode>> getEnabledMap() {
+    public static Map<OfflinePlayer, Pair<Location, GameMode>> getEnabledMap() {
         return enabled;
     }
 }
